3ebdd00dbd8c499b4880cc1261440a8638f9c24a
waykaray <waykaray@gmail.com>
2022-01-06 01:07:40 +0200

    hw15

---
+++ src/com/pb/andreiev/hw15/Client.java
@@ -0,0 +1,68 @@
+package com.pb.andreiev.hw15;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.Objects;
+
+public class Client extends JFrame {
+    JPanel jPanel;
+    JTextArea client;
+    JLabel label = new JLabel("Ответ от сервера");
+    JTextArea server;
+    JButton send = new JButton("Отправить");
+    Container cont;
+    public Client() {
+        cont = getContentPane();
+        setSize(600, 100);
+        setTitle("Многопоточный echo-сервер");
+        jPanel = new JPanel();
+        jPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        client = new JTextArea(1, 15);
+        server = new JTextArea(1, 5);
+        jPanel.add(client);
+        jPanel.add(send);
+        jPanel.add(label);
+        jPanel.add(server);
+        cont.add(jPanel);
+        send.addActionListener(e -> {
+            Socket s;
+            try {
+                s = new Socket("127.0.0.1", 1234);
+            } catch (IOException unknownHostException) {
+                unknownHostException.printStackTrace();
+                s = null;
+            }
+            if (s == null)
+                System.exit(-1);
+            BufferedReader in = null;
+            PrintWriter out = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                out = new PrintWriter(s.getOutputStream(), true);
+                out.flush();
+                String temp = client.getText();
+                out.println(temp);
+                server.setText(in.readLine());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } finally {
+                try {
+                    Objects.requireNonNull(out).close();
+                    in.close();
+                    s.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Client Client1 = new Client();
+            Client1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Client1.setVisible(true);
+        });
+    }
+}
---
+++ src/com/pb/andreiev/hw15/Server.java
@@ -0,0 +1,52 @@
+package com.pb.andreiev.hw15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    static class Handler implements Runnable {
+        private final Socket socket;
+        public Handler (Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                System.out.println(Thread.currentThread().getName() + ": Установлено входящее соединение");
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                String clientMessage;
+                System.out.println("Ожидаем сообщений");
+                while ((clientMessage = in.readLine()) != null) {
+                    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                    String formatDateTime = LocalDateTime.now().format(format);
+                    out.println(formatDateTime + ": " + clientMessage);
+                    System.out.println("Входящее сообщение: " + clientMessage);
+                }
+                out.close();
+                in.close();
+                socket.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        int port = 1234;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Сервер запущен на порту : " + port);
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            threadPool.submit(new Handler(clientSocket));
+        }
+    }
+}
